d3a56f9a777da4cd3fc7826ab411d9820ecc54e2
Miguel Ángel MC <mamoca28@hotmail.es>
2025-10-24 21:56:50 +0200

    feat: Implementada primera versión del carrito
    Faltaría modificar las cards de los productos para que se vean igual que los de Mercadona

--- Prácticas/Lab02/routes/router_tienda.js
+++ Prácticas/Lab02/routes/router_tienda.js
@@ -5,12 +5,13 @@ const router = express.Router();
 
 router.get("/", async (req, res) => {
   try {
-    // Get cart item count from session
-    const cart_item_count = req.session.cart.length;
+    // Get cart items from session
+    const cart_items = req.session.cart;
+    console.log("Cart items:", cart_items);
 
     // Fetch multiple products (array) for the template loop.
     const products = await Product.find({}).limit(8).lean();
-    res.render('portada.html', { products, cart_item_count });
+    res.render('portada.html', { products, cart_items, cart_item_count: cart_items.length });
   } catch (err) {
     console.error(err);
     res.status(500).send(err);
@@ -19,54 +20,34 @@ router.get("/", async (req, res) => {
 
 router.get("/search-results", async (req, res) => {
   try {
-    // Get cart item count from session
-    const cart_item_count = req.session.cart.length;
+    // Get cart items from session
+    const cart_items = req.session.cart;
 
     // Search products by 'img_alt' field using case-insensitive substring match
     const query = req.query.q || '';
     const products = await Product.find({ img_alt: { $regex: query, $options: 'i' } }).lean();
     const size = products.length;
-    res.render('search_results.html', { products, size, query, cart_item_count });
+    res.render('search_results.html', { products, size, query, cart_items, cart_item_count: cart_items.length });
   } catch (err) {
     console.error(err);
     res.status(500).send(err);
   }
 })
 
-//router.get("/cart", async (req, res) => {
-//  try {
-//    if (!req.session.cart) {
-//      req.session.cart = [];
-//    } else {
-//      const productsInCart = req.session.cart;
-//      const products = await Product.find({ _id: { $in: productsInCart } }).lean();
-//      res.render('cart.html', { products });
-//    }
-//  } catch (err) {
-//    console.error(err);
-//    res.status(500).send(err);
-//  }
-//})
-
 router.get("/cart/:id", async (req, res) => {
   try {
-    // Initialize cart if it doesn't exist
-    if (!req.session.cart) {
-      req.session.cart = [];
+    // Check if product exists in database
+    const id = req.params.id;
+    const product = await Product.findById(id).lean();
+    if (!product) {
+      return res.status(404).send({ message: "Unavailable product" });
     } else {
-      const id = req.params.id;
-      const product = await Product.findById(id).lean();
-      // Check if product exists in database
-      if (!product) {
-        return res.status(404).send({ message: "Unavailable id" });
+      // Add product to cart or update quantity
+      const already_in_cart = req.session.cart.find((item) => item.id == id);
+      if (!already_in_cart) {
+        req.session.cart.push({"id" : id, "product": product, "quantity" : 1});
       } else {
-        // Add product to cart or update quantity
-        const product = req.session.cart.find((item) => item.id == id);
-        if (!product) {
-          req.session.cart.push({"id" : id, "quantity" : 1});
-        } else {
-          product.quantity += 1;
-        }
+        already_in_cart.quantity += 1;
       }
     }
     res.redirect(req.get('referer'));
